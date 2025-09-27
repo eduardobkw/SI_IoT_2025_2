fead91c4a833cf235d80ea28b6a8a6b876e3acad
Massaki Igarashi <154282630+igarashimassaki@users.noreply.github.com>
2025-09-27 16:15:44 -0300

    Update MeuApp.py

--- Dia_06/StreamlitDashboard/MeuApp.py
+++ Dia_06/StreamlitDashboard/MeuApp.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.title("Hello Massaki")
+
+st.header('st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
